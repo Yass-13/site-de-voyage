3769cb24cb508ad37d3599f2836168f4371da6db
yassine <yassinemarbouh@yahoo.fr>
2021-10-26 16:24:38 +0200

    page contact

--- aviontournant.php
+++ aviontournant.php
@@ -4,10 +4,14 @@
 <head>
     <title>avion</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="./styleavion.css">
+    <link rel="stylesheet" href="./styleAvion.css">
     <link rel="stylesheet" href="./NavBarStyle.css">
   <link rel="stylesheet" href="./FooterStyle.css">
   <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
 </head>
 
 <body>
@@ -17,16 +21,16 @@
     <img src="./img/avion.png" id="img" onclick="shovDiv()">
 
    
-     <div id="rotate"></div>
+     <div id="rotate" class="animate__animated animate__zoomIn animate__delay-2s"></div>
     
      
 
 
-    <div id="niketamere">
-        <h2 id="zebi"></h2><br>
+    <div id="Plane">
+        <h2 id="Gain"></h2><br>
         <img src="./img/avion gain.png" class="fly">
     </div>
-    <button style="display: none;" id="replay" value="Refresh Page" onClick="window.location.reload()">replay</button>
+    <button style="display: none;" id="replay" class="animate__animated animate__zoomIn animate__delay-2s" value="Refresh Page" onClick="window.location.reload()">replay</button>
     <script>
         document.getElementById("img").addEventListener("click", function () {
             myfunction()
@@ -51,13 +55,13 @@
             var randomItem = myArray[Math.floor(Math.random() * myArray.length)];
 
             document.getElementById("rotate").innerHTML = randomItem;
-            document.getElementById("zebi").innerHTML = randomItem;
+            document.getElementById("Gain").innerHTML = randomItem;
             document.getElementById("img").style.transform = "rotate(1080deg)";
 
         }
 
         function shovDiv() {
-            setInterval(function () { document.getElementById("niketamere").style.display = "block"; }, 1000);
+            setInterval(function () { document.getElementById("Plane").style.display = "block"; }, 2000);
             setInterval(function () { document.getElementById("replay").style.display = "block"; }, 1000);
 
         }
